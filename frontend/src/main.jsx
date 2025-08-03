21dce32f34cf59d1c5c7e1c82ef7fe447e78d322
Amol M <amolmaheshwara270@gmail.com>
2025-08-04 00:30:50 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
-console.log("Domain:", domain);
-console.log("Client ID:", clientId);
-
 
 createRoot(document.getElementById('root')).render(
     <Auth0Provider
